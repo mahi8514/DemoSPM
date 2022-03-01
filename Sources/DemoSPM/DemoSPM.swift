9d86483ea234fd94be1fc0ebd1c99d4a86a62821
mahin <mahin.ibrahim@reasonlabs.com>
2022-03-01 15:15:29 +0400

    update for cocoapods

--- Sources/DemoSPM/DemoSPM.swift
+++ Sources/DemoSPM/DemoSPM.swift
@@ -1,5 +1,5 @@
 public struct DemoSPM {
-    public private(set) var text = "Hello, World! Its my first test spm framework!!!!! and there is an update"
+    public private(set) var text = "Hello, World! Its my first test spm framework!!!!! and there is an update and there is an update again for cocoapods"
 
     public init() {
     }
